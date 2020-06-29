98749b25b16a916eb767e3d9a870469b9ccd0456
Rafael Santos <rafaneng@gmail.com>
2020-06-29 09:11:34 -0300

    files created by first and second topic

---
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -0,0 +1,8 @@
+package bot;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is Jarvis");
+        System.out.println("I was created in 2020");
+    }
+}
