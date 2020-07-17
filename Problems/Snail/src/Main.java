66930ac4606003b9d1e0050d964311ce705e7112
Rafael Santos <rafaneng@gmail.com>
2020-07-16 21:26:03 -0300

    More some of exercises accomplished in the first stage

---
+++ Problems/Snail/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int h = scanner.nextInt();
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        int dayReachTheTop = (h - b - 1) / (a - b) + 1;
+
+        System.out.println(dayReachTheTop);
+
+    }
+}
